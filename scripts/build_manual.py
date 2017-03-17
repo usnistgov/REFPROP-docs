3687a2e330314642cee580f8895abe9d8e72f137
Ian Bell <ian.bell@nist.gov>
2017-03-17 13:43:57 -0600

    Update to add some preliminary parameter parsing

--- scripts/build_manual.py
+++ scripts/build_manual.py
@@ -32,7 +32,7 @@ def generate_manual_content(FOR_path, JSON_ofname = None):
     joined = ''
     for for_file in for_files:
         with open(for_file,'r') as fp:
-            joined += fp.read() + '\n'    
+            joined += fp.read() + '\n'  
 
     # Munge function arguments as needed to replace arguments that span lines
     isubstart = 0 
@@ -67,8 +67,8 @@ def generate_manual_content(FOR_path, JSON_ofname = None):
             break
 
     ####### Uncomment next lines to see what it did
-    # with open('joined.txt','w') as fp:
-    #     fp.write(joined)
+    with open('joined.txt','w') as fp:
+        fp.write(joined)
 
     objects = {}
 
@@ -81,7 +81,7 @@ def generate_manual_content(FOR_path, JSON_ofname = None):
             objlines = []
             for j in range(100):
                 if i+j == len(lines): break
-                if "ritten by" in lines[i+j] or lines[i+j] == ' ':
+                if "ritten by" in lines[i+j] or ' include ' in lines[i+j] or lines[i+j] == ' ':
                     break
                 else:
                     objlines.append(lines[i+j])
@@ -95,18 +95,65 @@ def generate_manual_content(FOR_path, JSON_ofname = None):
             json.dump(objects, fp, indent =2)
     return objects
 
+def deconstruct_function_info(contents):
+
+    def line_containing(s, search):
+        o = None
+        for iline, line in enumerate(s):
+            if search in line:
+                return iline
+        return o
+    def line_decomment(line):
+        if line == 'c': return ''
+        if line.startswith('c '):
+            return line[2::].strip()
+        else:
+            return line.strip()
+
+    # Determine how many lines correspond to the string information about the function
+    # Things that end this block are: *written*
+
+    # Find the line that contains Input and written lines
+    iInputs = line_containing(contents, 'Input')
+    iOutput = line_containing(contents, 'Output')
+
+    class struct(object): pass
+    info = struct()
+    info.doc_block = '\n\n'+ ' '*4+'XXXXXXXXXXXXXXXXXXXX  CANNOT GET DOCS\n\n'
+    info.in_args = {}
+    info.out_args = {}
+    if iInputs is not None and iOutput is not None:
+        info.doc_block = '\n'.join([' '*4+line_decomment(line) for line in contents[1:iInputs]])
+        for line in contents[iInputs+1:iOutput]:
+            if '--' in line:
+                arg, desc = line_decomment(line).strip().split('--')
+                info.in_args[arg.strip().lower()] = desc
+        for line in contents[iOutput+1::]:
+            if '--' in line:
+                arg, desc = line_decomment(line).strip().split('--')
+                info.out_args[arg.strip().lower()] = desc
+    print(info.in_args)
+    return info
+
 def parse_manual_contents(contents, function_dict, dll_functions):
     """
     Write the RST data needed by the sphinx-fortran package
     """
+    def chop_dll(s):
+        if s.endswith('dll'):
+            return s[0:-3]
+        else:
+            return s
+    # Functions that end with dll in the PASS_FTN.FOR file
     has_dll = [funcname for funcname, lines in six.iteritems(contents) if funcname.endswith('dll')]
-    missing = [func.rstrip('dll') for func in has_dll if func.rstrip('dll') not in contents]
+    # 
+    missing = [chop_dll(func) for func in has_dll if chop_dll(func) not in contents]
 
     sout = ''
     for func in sorted(has_dll):
 
         # Get remapped name, or the same dll-stripped name back again otherwise
-        func = name_remapping.get(func.rstrip('dll'), func.rstrip('dll'))
+        func = name_remapping.get(chop_dll(func), chop_dll(func))
 
         # Format the output
         something_outputted = False
@@ -116,14 +163,24 @@ def parse_manual_contents(contents, function_dict, dll_functions):
             print('skipping missing function:', func)
             continue
 
+        info = deconstruct_function_info(contents[func].split('\n'))
+
         args_string = ''
         if func+'dll' in function_dict:
             args = function_dict[func+'dll']['argument_list']
             for arg in args:
+                # arg is (name, type, length) or (name, type)
+                docs = None
+                if arg[0] in info.in_args:
+                    docs = '[INPUT] ' + info.in_args[arg[0]]
+                if arg[0] in info.out_args:
+                    docs = '[OUTPUT] ' + info.out_args[arg[0]]
+                if docs is None: 
+                    docs = 'XXXXXXXXXX'
                 if len(arg) == 3 and arg[2] > 0:
-                    args_string += ' '*4 + ':p ' + arg[1].strip('*') + arg[0] + '(' + str(arg[2]) + '): docs\n'
+                    args_string += ' '*4 + ':p ' + arg[1].strip('*') + arg[0] + '(' + str(arg[2]) + '): '+docs+'\n'
                 else:
-                    args_string += ' '*4 + ':p ' + arg[1].strip('*') + arg[0] + ': docs\n'
+                    args_string += ' '*4 + ':p ' + arg[1].strip('*') + arg[0] + ': '+docs+'\n'
             if dll_functions:
                 for arg in function_dict[func+'dll']['string_arguments']:
                     args_string += ' '*4 + ':p int ' + arg[0] + ': length of variable ``' + arg[0].replace('_length','') + '`` (default: '+str(arg[1])+')\n'
@@ -131,42 +188,29 @@ def parse_manual_contents(contents, function_dict, dll_functions):
             print('Cannot find function arguments for:', func+'dll')
             continue
 
-        # Build up the documentation block above the list of parameters
-        for iline, line in enumerate(contents[func].split('\n')):
-            # Remove leading 'c '
-            if line[0:2] == 'c ' or line == 'c':
-                theline = line[2::]
-            else:
-                theline = line
-            #print( theline)
+        # This is the definition of the function prototype
+        theargs = ', '.join([a[0] for a in function_dict[func+'dll']['argument_list']])
+        # Add on the arguments at the end with the string lengths if you are documenting the DLL
+        if dll_functions:
+            theargs += ', ' + ', '.join([a[0] for a in function_dict[func+'dll']['string_arguments']])
+        sout += '.. f:subroutine:: {func:s} ({args:s})\n\n'.format(func=func+'dll', args = theargs)
 
-            if iline == 0:
-                theargs = ', '.join([a[0] for a in function_dict[func+'dll']['argument_list']])
-                if dll_functions:
-                    theargs += ', ' + ', '.join([a[0] for a in function_dict[func+'dll']['string_arguments']])
-                sout += '.. f:subroutine:: {func:s} ({args:s})\n\n'.format(func=func+'dll', args = theargs)
-            elif iline < 10:
-                ll = theline.strip()
-                if ll:
-                    sout += ' '*4 + ll + '\n'
-                    something_outputted = True
-                else:
-                    if not something_outputted:
-                        continue # It's ok, just a leading blank line, keep going
-                    else:
-                        break # Done, empty line found
-            else:
-                pass
+        # The block of documentation
+        sout += info.doc_block
+
+        # The list of 
 
         sout += '\n'
         sout += args_string
         sout += '\n\n'
+        
     return sout
 
 if __name__=='__main__':
     FOR_path = 'R:\\914FILES'
 
     # Use the script in REFPROP-headers repo to generate a dictionary of function parameter data
+    # from the functions defined in the PASS_FTN.FOR file
     # ------------------------------------------------------------------------------------------
     sys.path.append('../externals/REFPROP-headers')
     import generate_header
@@ -189,4 +233,7 @@ if __name__=='__main__':
        fp.write(rst)
 
     import subprocess
-    subprocess.call('make html', cwd='qs', shell = True, stdout = sys.stdout, stderr = sys.stderr)
+    subprocess.call('make html', cwd='sphinx', shell = True, stdout = sys.stdout, stderr = sys.stderr)
+    subprocess.call('make latex', cwd='sphinx', shell = True, stdout = sys.stdout, stderr = sys.stderr)
+    for i in range(3):
+        subprocess.call('pdflatex REFPROP.tex', cwd='sphinx/_build/latex', shell = True, stdout = sys.stdout, stderr = sys.stderr)
---
+++ scripts/sphinx/index.rst
@@ -0,0 +1,19 @@
+.. REFPROP documentation master file, created by
+   sphinx-quickstart on Mon Aug 22 13:16:43 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to REFPROP's documentation!
+===================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   
+
+Code Documentation
+==================
+
+.. include:: ../for_docs.rst
