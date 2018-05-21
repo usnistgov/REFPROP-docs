69b5b61d1dbb498ec8025eaf7dfad5d8a3a3923d
Ian Bell <ian.bell@nist.gov>
2018-05-21 13:45:09 -0600

    Update author listing

--- doc/conf.py
+++ doc/conf.py
@@ -49,8 +49,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'REFPROP'
-copyright = u'2018, ib, ewl, etc.'
-author = u'EWL, IHB, etc.'
+copyright = u'2018'
+author = u'EWL, IHB, MH, MML'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -232,7 +232,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'REFPROP.tex', u'REFPROP Documentation',
-     u'ib, ewl, etc.', 'manual'),
+     u'EWL, IHB, MH, MML', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
